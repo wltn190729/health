642aa77d24448f996aaa5d4079642d216752ed79
최지수 <wltn190729@gmail.com>
2024-01-05 22:27:18 +0900

    전문가 클래스 DietExpert 생성 및 전문가 인터페이스 ExpertSolution 생성

---
+++ app/Services/DietExpert.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\ExpertSolution;
+
+class DietExpert implements ExpertSolution
+{
+
+    public function provideSolution(array $lifeStyleTags = [])
+    {
+        // TODO: Implement provideSolution() method.
+    }
+
+    public function setPriority()
+    {
+        // TODO: Implement setPriority() method.
+    }
+}
